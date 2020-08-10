5a57b0406c61762ec5ef1854026c3ee43477d746
lycheelychee <1811210166@pku.edu.cn>
2020-08-10 10:49:24 +0800

    plot

---
+++ plot2.R
@@ -0,0 +1,9 @@
+##### plot2 #####
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# plot2
+png(filename = './plot2.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+dev.off()
